2c72ce801a40e260a483b54569a459bd496d51e0
rumastudy <shsg95@naver.com>
2023-09-27 09:38:37 +0900

    axios install, mainVisual Absolute Position

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+/* Library */
+import { styled } from "styled-components";
 
 /* Img Import */
 import mVisual01 from "./img/mainVisual01.png";
@@ -6,16 +9,58 @@ import mVisual02 from "./img/mainVisual02.png";
 import mVisual03 from "./img/mainVisual03.png";
 import mVisual04 from "./img/mainVisual04.png";
 
+const bgArr = [mVisual01, mVisual02, mVisual03, mVisual04];
+
 const MainBanner = () => {
+  const [slideLocate, setSlideLocate] = useState(0);
+  /* 0, 25, 50, 75 */
+  const slideRef = useRef();
+  useEffect(() => {
+    const slidePlay = setInterval(() => {
+      if (slideLocate >= 75) {
+        setSlideLocate(0);
+      } else {
+        setSlideLocate(slideLocate - 25);
+      }
+      console.log("이동 시도");
+      slideRef.current.style.transform = `translateX(${slideLocate})`;
+    }, 3000);
+    return clearInterval(slidePlay);
+  });
+
+  /* Styled-Components */
+  /* BG Slider */
+  const ImgSlider = styled.ul`
+    width: 400%;
+    height: 100vh;
+    transform: translate(${slideLocate});
+    transition: 550ms;
+    display: flex;
+    position: absolute;
+    top: 0;
+    left: 0;
+    & > li {
+      display: block;
+      width: 25%;
+      height: 100%;
+      & > img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: bottom;
+      }
+    }
+  `;
   return (
-    <section>
+    <section className="MainBanner">
       {/* Img Slider */}
-      <arcticle className="imgSlider">
-        <img src={mVisual01} alt="main_visual_img" />
-        <img src={mVisual02} alt="main_visual_img" />
-        <img src={mVisual03} alt="main_visual_img" />
-        <img src={mVisual04} alt="main_visual_img" />
-      </arcticle>
+      <ImgSlider ref={slideRef} className="imgSlider">
+        {bgArr.map((items) => (
+          <li>
+            <img src={items} alt="bg_img" />
+          </li>
+        ))}
+      </ImgSlider>
       <header>
         <button>RESERVATION</button>
         <button>BUTTER</button>
